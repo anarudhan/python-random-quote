95a68991eac7aae4b400a936b89093e3ba139be6
anarudhan <anarudhan.sathis@outlook.com>
2019-12-19 05:29:35 +0000

    Latest

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,5 @@
 import random
 
-<<<<<<< HEAD
 def main():
   #print("Keep it logically awesome.")
 
@@ -10,18 +9,6 @@ def main():
 
   last = 13
   rnd = random.rndint(0, last)
-=======
-def primary():
-  # print("Keep it logically awesome.")
-
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
-  last = 13
-  rnd = random.randint(0, last)
-
->>>>>>> 298d9fe54c21551aaf02a969687279dc2ffb255f
 
   print(quotes[rnd])
 
